462938b67f9b3088148767c71495c08f02f1d907
Datseris <datseris.george@gmail.com>
2024-02-27 09:27:14 +0000

    textbook bib entry

--- src/dynamicalsystems.jl
+++ src/dynamicalsystems.jl
@@ -51,7 +51,7 @@ end
 """
     dynamical_system_summary(ds::DynamicalSystem)
 
-Return a printable string containing a summary of `ds`,
+Return a printable/writable string containing a summary of `ds`,
 which outlines its current status and lists all symbolic
 equations and parameters that constitute the system, if a referrence to a
 ModelingToolkit.jl exists in `ds`.
