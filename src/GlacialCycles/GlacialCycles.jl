ba3679d5853f9df3bd5672ba3d7d98fffc2036b4
Datseris <datseris.george@gmail.com>
2024-05-07 15:44:41 +0100

    breaking change

--- src/GlacialCycles/GlacialCycles.jl
+++ src/GlacialCycles/GlacialCycles.jl
@@ -2,5 +2,11 @@ module GlacialCycles
 using ConceptualClimateModels
 
 # TODO
+for (root, dirs, files) in walkdir(joinpath(@__DIR__))
+    file == "GlacialCycles.jl" && continue
+    for file in files
+        include(joinpath(root, file))
+    end
+end
 
 end
