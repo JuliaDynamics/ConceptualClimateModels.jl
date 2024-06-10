4dd6b15a0e64e3dd7368030597e7c7895f2f93e6
Datseris <datseris.george@gmail.com>
2024-06-10 13:00:57 +0100

    improve sigmoid process

--- src/processes_advanced.jl
+++ src/processes_advanced.jl
@@ -4,7 +4,6 @@ export SigmoidProcess
 """
     SigmoidProcess <: Process
     SigmoidProcess(variable, driver; left, right, scale, reference)
-    SigmoidProcess(variable, driver; left, right, scale, start)
 
 A common process for when a `variable` has a sigmoidal dependence on a `driver` variable.
 The rest of the input arguments should be real numbers or `@parameter` named parameters.
@@ -15,8 +14,8 @@ variable ~ left + (right - left)*(1 + tanh(2(driver - reference)/scale))*0.5
 ```
 i.e., the variable goes from value `left` to value `right` as `driver` increases
 over a range of `scale` (centered at `reference`).
-Alternatively, you can provide `start` instead of `reference`,
-which creates the reference value as `reference = start - scale/2`.
+Instead of `reference` you may provide `start` or `finish` keywords,
+which make `reference = start + scale/2` or `reference = finish - scale/2` respectively.
 
 If the values given to the parameters of the expression are real numbers, they become
 named parameters prefixed with the name of `variable`, then the name of the `driver`,
@@ -32,12 +31,15 @@ struct SigmoidProcess <: Process
     reference
 end
 
-function SigmoidProcess(variable, driver; left=0, right=1, scale=1, reference=nothing, start=nothing)
-    if !isnothing(reference) && !isnothing(start)
-        error("Only one of `reference, start` can be given!")
+function SigmoidProcess(variable, driver; left=0, right=1, scale=1,
+    reference=nothing, start=nothing, finish = nothing)
+    if count(!isnothing, (reference, start, finish)) > 1
+        error("Exactly one of `reference, start, finish` must be given!")
     end
     if isnothing(reference)
-        reference = start - scale/2
+        reference = start + scale/2
+    elseif isnothing(finish)
+        reference = finish - scale/2
     end
     return SigmoidProcess(variable, driver, left, right, scale, reference)
 end
